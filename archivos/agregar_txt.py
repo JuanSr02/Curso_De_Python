feb71c2dfd249e327fb2cb599b5df159a5194b6c
JuanSr02 <juanma2002123@gmail.com>
2023-07-17 19:09:35 -0300

    Archivos, probando

--- archivos/agregar_txt.py
+++ archivos/agregar_txt.py
@@ -1,6 +1,4 @@
 with open('archivos\\texto_de_dalto.txt','a',encoding="UTF-8") as archivo:
     #usando un bucle para agregar varias lineas
-    archivo.write("\n")
-    for i in range(5):
+    for i in range(5): archivo.write(f"\nLinea {i+1} agregada")
         #agregando lineas
-        archivo.write(f"Linea {i+1} agregada\n")
